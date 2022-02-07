7313c1d145dce8e290368b7873063202a6257ba6
yeonank99@gmail.com <yeonj630@sookmyung.ac.kr>
2022-02-08 01:19:44 +0900

    temp

--- routes/connectDB.js
+++ routes/connectDB.js
@@ -14,7 +14,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log('Connected!');
+    console.log('database Connected!');
 });
 
 connection.query('SELECT * from User', (error, rows, fields) => {
